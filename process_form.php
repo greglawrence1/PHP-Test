6d96e5e3c640a451c8dfb5f99064ae806d7c23e0
Gregory Lawrence <greg_lawrence2017@outlook.com>
2024-07-24 16:36:18 +0100

    added a cool effect to the winning page

--- process_form.php
+++ process_form.php
@@ -1,9 +1,5 @@
 <?php
  
- ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-
     function LastDigit($number){
         $split = str_split($number);
         $last = end($split);

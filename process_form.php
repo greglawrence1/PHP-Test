a05d50ea349b30388457c50a36616ec813e9358b
Gregory Lawrence <greg_lawrence2017@outlook.com>
2024-07-24 16:16:58 +0100

    if the id is 0 or 5 they are redirected to a new page

--- process_form.php
+++ process_form.php
@@ -1,5 +1,15 @@
 <?php
  
+ ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    function LastDigit($number){
+        $split = str_split($number);
+        $last = end($split);
+        return $last;
+    }
+
     include "connection.php";
 
     if(isset($_POST["submit"])){
@@ -7,7 +17,7 @@
         $DOB = $_POST["DOB"];
         $password = $_POST["password"];
 
-        $query = "INSERT INTO users
+        $sql = "INSERT INTO users
              (Email, DOB , UserPassword)
              VALUES
              ('$email', '$DOB', '$password')   
@@ -15,11 +25,24 @@
 
         //echo $query;
        //exit;
-        mysqli_query($connection, $query);
-        if(is_Numeric(mysqli_insert_id($connection))){
-        header('location:form.php');
+        //mysqli_query($connection, $query);
+        
+        //header('location:form.php');
+
+        if(mysqli_query($connection, $sql)){
+            $last_id = mysqli_insert_id($connection);
+            echo "Records inserted successfully. Last inserted ID is: " . $last_id;
+
+            if(LastDigit($last_id) == 0 || LastDigit($last_id) == 5)
+                header('location:congrats.php');
+            else{
+                header('location:form.php');
+            }
+        } else{
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($connection);
         }
-            //echo "Records inserted successfully. Last inserted ID is: " . $last_id;
+        
+    
     }
     
 ?>

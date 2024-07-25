bb889c4d5fb0f44a251b9ca5d114936fc63f5cb0
Gregory Lawrence <greg_lawrence2017@outlook.com>
2024-07-25 12:06:06 +0100

    added an age validation

--- my.js
+++ my.js
@@ -33,14 +33,40 @@ function validateForm(event) {
         DOBError.className = "alert alert-danger";
         DOBError.style.display = "block";
         formValid = false;
-    }
+    }  
     
-    if (!dateFormat(DOBField.value)) {
-        DOBError.innerHTML = "Invalid DOB! Please enter in the format year-month-date for example 2000-12-03.";
+    else if (!dateFormat(DOBField.value)) {
+        DOBError.innerHTML = "Invalid DOB format! Please enter in the format year-month-date (e.g., 2000-12-03).";
         DOBError.className = "alert alert-danger";
         DOBError.style.display = "block";
         formValid = false;
+
+    } else {
+        let text = DOBField.value;
+        const myArray = text.split("-");
+        let year = parseInt(myArray[0]);
+        let month = parseInt(myArray[1]) - 1; 
+        let day = parseInt(myArray[2]);
+        let birthDate = new Date(year, month, day);
+        let today = new Date();
+
+        
+        let age = today.getFullYear() - birthDate.getFullYear();
+        let ageCalc = today.getMonth() - birthDate.getMonth();
+
+        if (ageCalc < 0 || (ageCalc === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+
+        if (age < 18) {
+            DOBError.innerHTML = "You must be at least 18 years old.";
+            DOBError.className = "alert alert-danger";
+            DOBError.style.display = "block";
+            formValid = false;
+        }
     }
+    
+   
 
     if (passwordField.value.length > 30 || passwordField.value.length < 5) {
         passwordError.innerHTML = "Invalid Password! Must be between 5 and 30 characters.";

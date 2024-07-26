8ae0fabf2bcb2f20c8d9b081f80a35885b867b6e
Gregory Lawrence <greg_lawrence2017@outlook.com>
2024-07-26 17:01:55 +0100

    made the form look better

--- form.php
+++ form.php
@@ -5,39 +5,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form</title>
+    <link rel="stylesheet" href="form.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body style="background-color:lightblue">
-    <h1>Raffle Form Registration</h1>
-
-    <form action="process_form.php" method="POST">
-        <div class="container p-5 my-5 bg-primary text-white">
-
-        <label for="firstname" class="form-label">Firstname:</label>
-        <input type="text" class="form-control" id="firstname" name="firstname">
-        <div id="firstnameError" class="alert"></div>
-
-        <label for="surname" class="form-label">Surname:</label>
-        <input type="text" class="form-control" id="surname" name="surname">
-        <div id="surnameError" class="alert"></div>
-
-        <span class="input-group-addon" id="sizing-addon1">@</span>
-        <label for="email" class="form-label">Email:</label>
-        <input type="text" class="form-control" id="email" name="email">
-        <div id="emailError" class="alert"></div>
-
-        <label for="DOB" class="form-label">Date of Birth Format: Year-Month-Day:</label>
-        <input type="text" class="form-control" id="DOB" name="DOB">
-        <div id="DOBError" class="alert"></div>
-
-        <label for="password" class="form-label">Password:</label>
-        <input type="password" class="form-control" id="password" name="password">
-        <div id="passwordError" class="alert"></div>
-    <br>
-        <button type="submit" name="submit" id="submit" class="btn btn-light">Submit</button>
+<body>
+    <div class="login-wrap">
+        <div class="login-html">
+            <label for="tab-1" class="tab">Raffle Form Registration</label>
+            <div class="login-form">
+                <div class="sign-in-htm">
+                    <form action="process_form.php" method="POST">
+                        <div class="group">
+                            <label for="firstname" class="label">Firstname:</label>
+                            <input type="text" class="input" id="firstname" name="firstname">
+                            <div id="firstnameError" class="alert"></div>
+                        </div>
+                        <div class="group">
+                            <label for="surname" class="label">Surname:</label>
+                            <input type="text" class="input" id="surname" name="surname">
+                            <div id="surnameError" class="alert"></div>
+                        </div>
+                        <div class="group">
+                            <label for="email" class="label">Email:</label>
+                            <input type="text" class="input" id="email" name="email">
+                            <div id="emailError" class="alert"></div>
+                        </div>
+                        <div class="group">
+                            <label for="DOB" class="label">Date of Birth (Year-Month-Day):</label>
+                            <input type="text" class="input" id="DOB" name="DOB">
+                            <div id="DOBError" class="alert"></div>
+                        </div>
+                        <div class="group">
+                            <label for="password" class="label">Password:</label>
+                            <input type="password" class="input" id="password" name="password">
+                            <div id="passwordError" class="alert"></div>
+                        </div>
+                        <div class="group">
+                            <button type="submit" name="submit" id="submit" class="button">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    </form>
-   
     <script src="my.js"></script>
 </body>
 </html>
